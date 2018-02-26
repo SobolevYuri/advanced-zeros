65f12239172558ddbc39ff8c07d0ecc1ad3d1f4e
SobolevYuri <yura.sobolev.91@gmail.com>
2018-02-26 11:25:10 +0300

    Fuh, it used to be easier...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports=function getZerosCount(number, base){
+  let zeroNumber=number, bs=base;
+  let current, sumCur, degr;
+  let i=2;
+
+while(i<=bs)
+  {
+    if(bs%i === 0){
+      current=number;
+      degr=sumCur=0;
+
+   while(current/i > 0){
+        current= Math.floor(current/i);
+        sumCur+= current;
+      }
+
+ while(bs%i === 0){
+        bs= bs/i;
+        degr++;
+      }
+
+if(Math.floor(sumCur/degr)<zeroNumber){
+  zeroNumber=Math.floor(sumCur/degr);
+}
+    }
+    i++;
+  }
+  return zeroNumber;
 }
